baf1ae05023711cd2dd86ccf2ab1dbc484595358
sofiigorevna <sofia.konchencko@yandex.ru>
2024-06-17 22:16:11 +0300

    Initial Commit

---
+++ myToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myToDoList
+//
+//  Created by 1234 on 17.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
